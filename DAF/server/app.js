775717527b7cb7ccaf9e421b948a4bbd300b4057
Adham <77929356+Adham27@users.noreply.github.com>
2023-09-14 10:37:25 +0300

    First commit

---
+++ DAF/server/app.js
@@ -0,0 +1,12 @@
+
+import initApp from './src/router.js'
+import express from 'express'
+
+const app = express();
+const port = 5000
+
+initApp(app,express);
+
+app.listen(port, ()=>{
+ console.log(`running on port....... ${port} `)
+})
